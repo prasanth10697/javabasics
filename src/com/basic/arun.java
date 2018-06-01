c7299f7d68b5794e3bd97e63adc1ea81fca345db
prasanth10697 <prasanthrgvp97>
2018-06-01 10:15:19 +0530

    Rectangle programming

--- src/com/basic/arun.java
+++ src/com/basic/arun.java
@@ -4,37 +4,15 @@ public class arun
 	public static void main(String s[])
 
 	{
-	
-	
-	int i = 1;
-	while ( i < 1 )
-	i++;
 
+		{
+			int i = 1;
+			while ( i < 1 )
+			i++;
 
-    int k;
-	for (k= 1; k != 2; k += 3 )
-	System.out.println( k );
-
-	int n = 2;
-	switch (n)
-	{
-	case 1:
-	System.out.println( "The number is 1" );
-	break;
-	case 2:
-	System.out.println( "The number is 2" );
-	break;
-	default:
-	System.out.println( "The number is not 1 or 2" );
-	break;
 	}
-	
-
-	{
-	n = 1;
-	while (n <= 10 )
-	System.out.println( n++ );
 	}
 }
 	
-} 
+
+	
