fde7526147fc7abad93e54104d04d753912eb263
prasanth10697 <prasanthrgvp97@gmail.com>
2018-07-21 13:03:27 +0530

    years programming

---
+++ src/com/basic/Year.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+import java.util.Collections;
+
+public class Year{
+   public static void main(String[] args) {
+      Scanner scnr=new Scanner(System.in);
+	  int n=scnr.nextInt();
+	  if(n>=2018)
+	  { 
+	    System.out.println("Yes");
+	  }else
+	  {
+	    System.out.println("No");
+	  }
+    }
+}
