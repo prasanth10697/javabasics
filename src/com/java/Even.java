54bc161edc4576ca1d365daf519770b5dff73b36
prasanth10697 <prasanthrgvp97@gmail.com>
2018-07-28 17:40:59 +0530

    Even numbers print

--- src/com/java/Even.java
+++ src/com/java/Even.java
@@ -1,12 +1,12 @@
 import java.util.*;
-public class Oddn{
+public class Even{
 	public static void main(String args[]){
 		Scanner scnr=new Scanner(System.in);
 		int num=scnr.nextInt();
 		int num2=scnr.nextInt();
-		for(int i=num;i<=num2;i++){
+		for(int i=num;i<num2;i++){
 			if(i%2==0){
-			 System.out.println(i);
+				System.out.println(i);
 			}
 		}
 	}
